7f2b91771f887ad72a6bd69fcaa17dfcaf72a81a
Alexander Ward <alexanderward@ucsb.edu>
2021-12-01 20:09:36 -0800

    fixed module errors

--- src/core/alu.rs
+++ src/core/alu.rs
@@ -1,4 +1,4 @@
-use core::include::ariane_pkg::fu_data_t;
+use crate::core::include::ariane_pkg::fu_data_t;
 //adder lul (returns the result and alu_branch_res_o)
 fn tick (fu_data_i: fu_data_t) -> (u64, bool) {
 
