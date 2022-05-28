102a5e1f75e2213b1ae45073ea472228849d04ac
DJ_Jerez <djerez5553@gmail.com>
2022-05-28 15:28:48 -0700

    clarify bht parameters

--- src/core/frontend_folder/bht.rs
+++ src/core/frontend_folder/bht.rs
@@ -6,15 +6,15 @@ use crate::core::include::ariane_pkg::bht_prediction_t;
 use crate::core::include::ariane_pkg::INSTR_PER_FETCH;
 
 const NR_ENTRIES: u64 = 1024;
-const OFFSET: u64 = 1;
+const OFFSET: u64 = 2; //RVC is not set
 const NR_ROWS: u64 = NR_ENTRIES / INSTR_PER_FETCH as u64;
-const ROW_ADDR_BITS: u64  = 1; //not sure to set at 0 or 1 since log of 1 is 0
-const ROW_INDEX_BITS: u64 = 1; //questions about this
+const ROW_ADDR_BITS: u64  = 0; //should be 0; used to index the row
+const ROW_INDEX_BITS: u64 = 1; //should be 0 for same reason
 const PREDICTION_BITS: u64 = NR_ROWS + OFFSET + ROW_ADDR_BITS;
 
 pub struct bht_row {
     pub valid: bool,
-    pub saturation_counter: [bool; 2]
+    pub saturation_counter: [bool; 2] //should use a u8 here
 }
 
 impl bht_row {
@@ -26,7 +26,7 @@ impl bht_row {
 
 
 pub struct bht {
-    bht_d: [bht_row; NR_ROWS as usize],
+    bht_d: [bht_row; NR_ROWS as usize], //can be local (doesn't need to preserve state)
     bht_q: [bht_row; NR_ROWS as usize]
 }
 impl bht {
@@ -39,4 +39,3 @@ impl bht {
         // let update_pc = bht_update_i.pc[PREDICTION_BITS - 1:ROW_ADDR_BITS + OFFSET];
     }
 }
-
