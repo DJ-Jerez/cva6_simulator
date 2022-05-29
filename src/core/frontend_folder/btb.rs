2ca9c3b8b9270762ca63f80ed70bd034d2cb7e16
DJ_Jerez <djerez5553@gmail.com>
2022-05-29 00:34:18 -0700

    add btb new, update functionality (no sequential operation)

--- src/core/frontend_folder/btb.rs
+++ src/core/frontend_folder/btb.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+
 use crate::core::include::ariane_pkg::{
     btb_update_t,
     btb_prediction_t
@@ -6,31 +9,59 @@ use crate::core::include::ariane_pkg::{
 const NR_ENTRIES: u64 = 8;
 const NR_ROWS: u64 = NR_ENTRIES;
 const OFFSET: u64 = 1;
-const ROW_ADDR_BITS: u64 = 1;
+const ROW_ADDR_BITS: u64 = 0;
 const ROW_INDEX_BITS: u64 = 1;
 const PREDICTION_BITS: u64 = 3 + OFFSET + ROW_ADDR_BITS; //3 is clog2(NR_ROWS)
 const ANTIALIASING_BITS: u64 = 8;
 
 pub struct btb{
-    btb_d: [btb_prediction_t; NR_ROWS as usize],
-    btb_q: [btb_prediction_t; NR_ROWS as usize],
+    btb_d: [btb_prediction_t; NR_ROWS as usize], //input; could just update q instead
+    btb_q: [btb_prediction_t; NR_ROWS as usize], //output
 
 
 }
 
 impl btb {
     //  implement copy trait on btb_prediction_t before uncommenting
-    // pub fn new() -> btb {
-    //     let init_bpt: btb_prediction_t = btb_prediction_t::new(false, 0);
-    //     btb {
-    //         btb_d: [init_bpt; NR_ROWS as usize],
-    //         btb_q: [init_bpt; NR_ROWS as usize]
-    //     }
-    // }
+    pub fn new() -> btb {
+        let init_bpt: btb_prediction_t = btb_prediction_t::new(false, 0);
+        btb {
+            btb_d: [init_bpt; NR_ROWS as usize],
+            btb_q: [init_bpt; NR_ROWS as usize]
+        }
+    }
 
-    pub fn tick(reset_ni: bool, flush_i: bool, debug_mode_i: bool, vpc_i: u64, btb_update_i: btb_update_t) -> btb_prediction_t {
+    pub fn update(&mut self, btb_update_i: &btb_update_t, update_pc: u64) {
+        println!("Update:");
+        if btb_update_i.valid {
+            self.btb_d[update_pc as usize].valid = true;
+            self.btb_d[update_pc as usize].target_address = btb_update_i.target_address;
+            println!("Valid: {0}, Target Address: {1}", self.btb_d[update_pc as usize].valid, self.btb_d[update_pc as usize].target_address);
+        }
+    }
+
+    pub fn tick(&mut self, reset_ni: bool, flush_i: bool, debug_mode_i: bool, vpc_i: u64, btb_update_i: btb_update_t) -> btb_prediction_t {
+        // workaround; grabbing only the bits used for indexing the btb
+        let index: u64 = (vpc_i % (1 << PREDICTION_BITS)) >> (ROW_ADDR_BITS + OFFSET);
+        println!("Index: {}", index);
+        println!("Prediction bits: {}", PREDICTION_BITS);
+        println!("ROW_ADDR_BITS + OFFSET: {}", ROW_ADDR_BITS + OFFSET);
+        let update_pc: u64 = (btb_update_i.pc % (1 << PREDICTION_BITS)) >> (ROW_ADDR_BITS + OFFSET);
+        println!("Update pc: {}", update_pc);
+
+        //update the table on mis-predict
+        self.update(&btb_update_i, update_pc);
+
+        //do sequential operation
+        // if !reset_ni {
+        //
+        // }
+
+        //give prediction
+        self.btb_d[index as usize]
+
         //stub
-        btb_prediction_t::new(false, 0)
+        // btb_prediction_t::new(false, 0)
 
         //make sure to use mutable references when updating btb
     }
